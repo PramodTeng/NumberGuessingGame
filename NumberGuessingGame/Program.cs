7cc7894acbe2b8e56d42027f4e453a1c9e935576
Pramod Dhimal <107398819+PramodTeng@users.noreply.github.com>
2025-03-13 11:18:18 +0545

    Refacto

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -6,79 +6,52 @@ namespace NumberGuessingGame
     {
         private static void Main(string[] args)
         {
-            //Greet user
+            // Greet user
             Console.WriteLine("Welcome contestant!");
 
-            //Create random number
+            // Create random number
             var numberToGuess = new Random().Next(0, 100);
 
-            //Ask user for a number
-            var userGuessedNumber = AskForNumber(numberToGuess);
+            //Create  invalid guessed number
+            var userGuessedNumber = -1;
 
-            //Check the result
-            CheckResult(numberToGuess, userGuessedNumber);
-        }
-
-        /// <summary>
-        /// Ask user to guess a number
-        /// </summary>
-        /// <param name="numberToGuess">The number to guess by the user</param>
-        /// <returns>the user's guessed number</returns>
-        public static int AskForNumber(int numberToGuess)
-        {
-            //Create checked number with initial out of range value;
-            var number = int.MaxValue;
-
-            //until the number is valid
-            while (number == int.MaxValue)
+            // Until the user guessed the right number
+            while (userGuessedNumber != numberToGuess)
             {
-                //Tell user to guess a number what he is thinking
+                // Tell user to guess a number
                 Console.WriteLine("Take a guess what I'm thinking... Any number between 0 and 100");
 
-                //Read user input
-                var userGuessedNumber = Console.ReadLine();
+                // Read user input
+                var userInput = Console.ReadLine();
 
-                //If the user entered a valid number
-                if (int.TryParse(userGuessedNumber, out var guessednumber))
-                    //Set the checked number to the guessed number
-                    number = guessednumber;
+                // If the user entered a valid number
+                if (int.TryParse(userInput, out userGuessedNumber))
+                {
+                    if (userGuessedNumber == numberToGuess)
+                    {
+                        // Congratulate the user for guessing the right number
+                        Console.WriteLine($"Well done!!! You guessed right. I was thinking of {userGuessedNumber}.");
+                        break;
+                    }
+                    else
 
-                //Otherwise...
+                    {
+                        // Check the user's guess
+                        var a = userGuessedNumber < numberToGuess ? "higher" : "lower";
+                        Console.WriteLine($"Not quite. I'm thinking of a number that is {a}.");
+                    }
+                }
                 else
-                    //Inform user they entered an invalid number
-                    Console.WriteLine("Please enter a valid number");
-            }
-            return number;
-        }
-
-        /// <summary>
-        /// Check the user's guess Result
-        /// </summary>
-        /// <param name="numberToGuess">the number to guess by user</param>
-        /// <param name="userGuessedNumber">the user guess number</param>
-
-        public static void CheckResult(int numberToGuess, int userGuessedNumber)
-        {
-            //until the user guessed the right number
-            while (numberToGuess != userGuessedNumber)
-            {
-                //check the user's guess if it is lower
-                if (userGuessedNumber < numberToGuess)
                 {
-                    //Inform user with hint that number is higher
-                    Console.WriteLine("Not quite. I'm thinking of a number that is higher ");
+                    // Inform user they entered an invalid number
+                    Console.WriteLine("Please enter a valid number.");
                 }
-                //check the user's guess if it is higher
-                else if (userGuessedNumber > numberToGuess)
-                {
-                    //Inform user with hint that number is lower
-                    Console.WriteLine("Not quite. I'm thinking of a number that is lower ");
-                }
-                // Ask for another guess
-                userGuessedNumber = AskForNumber(numberToGuess);
             }
-            //Congratulate the user for guessing the right number
-            Console.WriteLine($"Well done!!! you guessed right. I was thinking of {userGuessedNumber}. Thanks for playing");
+
+            //Thank user for playing
+            Console.WriteLine("Thanks for Playing");
+
+            Console.ReadLine();
         }
     }
 }
